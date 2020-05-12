c1bc8a72a43f78b9870d81bc17e2f4832a8a1fd9
Oscar Arango <oscar.arango326@gmail.com>
2020-05-12 02:51:11 +0200

    initial commit Admin template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
